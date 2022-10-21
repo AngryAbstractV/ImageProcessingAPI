4a3bed2c9f170c71afc5271fc1e8e270d93217be
OutbreakSource <daniel_ends@yahoo.com>
2022-10-20 21:13:44 -0600

    -setting get index to put out random scores

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 from pydantic import BaseModel
 from starlette.responses import HTMLResponse
 from typing import List
+
 origins = ["*"]
 
 app = FastAPI()
@@ -19,9 +20,14 @@ app.add_middleware(
 )
 from fastapi import UploadFile, File
 
+
 @app.get("/")
 async def main():
-    return upload_image
+    return {"Scores": [random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100]}
+
 
 @app.post("/upload")
 async def upload_image(user_id: int, file: UploadFile = File(...)):
@@ -29,5 +35,3 @@ async def upload_image(user_id: int, file: UploadFile = File(...)):
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100,
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100,
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100]}
-
-
