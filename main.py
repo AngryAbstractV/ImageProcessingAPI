9160bfd097dfcc639c36183e483fccafaec03624
OutbreakSource <daniel_ends@yahoo.com>
2022-10-20 21:07:06 -0600

    -taking image in and posting fake scores

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 import random
-
+import cv2
+import numpy as np
+from pydantic import BaseModel
+from starlette.responses import HTMLResponse
+from typing import List
 
 app = FastAPI()
 
+from fastapi import UploadFile, File
 
-@app.get("/")
-def hello():
 
-    return {"Scores": [random.randint(10, 100) / 100,random.randint(10, 100) / 100,
-                        random.randint(10, 100) / 100,random.randint(10, 100) / 100,
-                        random.randint(10, 100) / 100,random.randint(10, 100) / 100,
-                        random.randint(10, 100) / 100,random.randint(10, 100) / 100]}
+@app.post("/upload_image/{user_id}")
+async def upload_image(user_id: int, file: UploadFile = File(...)):
+    return {"Scores": [random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100,
+                       random.randint(10, 100) / 100, random.randint(10, 100) / 100]}
