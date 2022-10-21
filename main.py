8281136ec2f5c6f6e84b5b60bde4d65f2514a858
OutbreakSource <daniel_ends@yahoo.com>
2022-10-20 21:55:34 -0600

    -removing everything for now

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 import random
-from fastapi.middleware.cors import CORSMiddleware
-import cv2
-import numpy as np
-from pydantic import BaseModel
-from starlette.responses import HTMLResponse
-from typing import List
-
-origins = ["*"]
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/")
