f8b16c9964891e930141e81de6abce4ecf87462d
Patrick <pmdino93@gmail.com>
2022-10-28 12:19:54 -0600

    /upload method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import random
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
+import cv2
+import io
 
 
 middleware = [
@@ -24,3 +26,12 @@ async def main():
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100,
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100,
                        random.randint(10, 100) / 100, random.randint(10, 100) / 100]}
+
+
+
+@app.post("/upload")
+async def root(file: UploadFile = File(...)):
+        # request_object_content = await file.read()
+        # img = cv2.imread(io.BytesIO(request_object_content))
+        # firstPixel = img[0][0]
+        return 'Poggers'
