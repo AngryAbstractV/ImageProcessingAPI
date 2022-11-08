9960e68a13e8e6a5eea29fa7117a864c4545ded8
OutbreakSource <daniel_ends@yahoo.com>
2022-11-07 17:56:05 -0700

    - changing format of CORS - Old bug is still there

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ from scipy.signal import argrelextrema
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
