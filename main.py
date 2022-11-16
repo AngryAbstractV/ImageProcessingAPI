60e1fc58cb27689c1a927acc525d1e7108f93f17
Patrick <pmdino93@gmail.com>
2022-11-15 17:08:42 -0700

    fix image undersize error

--- main.py
+++ main.py
@@ -35,10 +35,13 @@ async def root(file: UploadFile = File(...)):
     properties_list = [0] * 6
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    processedIMG = preprocessing(img)
-    properties_list[2] = calcHarmony(processedIMG)
-    properties_list[3] = calcVariety(processedIMG)
-    return properties_list
+    painting = Painting(img)
+    painting.preprocessing()
+    painting.calculateProperties()
+    # processedIMG = preprocessing(img)
+    # properties_list[2] = calcHarmony(processedIMG)
+    # properties_list[3] = calcVariety(processedIMG)
+    return painting.properties_list
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
@@ -60,34 +63,90 @@ def pred(img):
     return predictions[0]
 
 
-def preprocessing(img):
-    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+class Painting:
 
-    # Scale image, preserving aspect ratio
-    pix_threshold = 500
-    img_wid = hsv_img.shape[1]
-    img_len = hsv_img.shape[0]
-    new_wid = 0
-    new_len = 0
+    # init method or constructor
+    def __init__(self, img):
+        # self.name = name
+        # self.imageAddress = imageAddress
+        self.properties_list = [0] * 6
+        self.img = img
+        self.hsv_img = np.zeros((1, 1, 1), dtype=np.int32)
 
-    if img_wid > pix_threshold or img_len > pix_threshold:
-        # find out which is bigger
-        if img_wid > img_len:
-            new_wid = pix_threshold
-            scale = (new_wid * 100) // img_wid
-            new_len = (img_len * scale) // 100
-        elif img_len > img_wid:
-            new_len = pix_threshold
-            scale = (new_len * 100) // img_len
-            new_wid = (img_wid * scale) // 100
-        else:
-            # wid and len are equal
-            new_wid = pix_threshold
-            new_len = pix_threshold
+    def preprocessing(self):
+        # pre-processesing on image
 
-        dim = (new_wid, new_len)
-        hsv_img = cv2.resize(hsv_img, dim, interpolation=cv2.INTER_AREA)
-        return hsv_img
+        # input image
+        # self.img = cv2.imread(self.imageAddress, 1)
+
+        self.hsv_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
+        # hsv_img is a 3d numpy array with the inner array having values:
+        # HUE = 0 - 180 (179?)
+        # SATURATION = 0 - 255
+        # VALUE = 0 - 255
+
+        # Scale image, preserving aspect ratio
+        pix_threshold = 500
+        img_wid = self.hsv_img.shape[1]
+        img_len = self.hsv_img.shape[0]
+        new_wid = 0
+        new_len = 0
+
+        if img_wid > pix_threshold or img_len > pix_threshold:
+            # find out which is bigger
+            if img_wid > img_len:
+                new_wid = pix_threshold
+                scale = (new_wid * 100) // img_wid
+                new_len = (img_len * scale) // 100
+            elif img_len > img_wid:
+                new_len = pix_threshold
+                scale = (new_len * 100) // img_len
+                new_wid = (img_wid * scale) // 100
+            else:
+                # wid and len are equal
+                new_wid = pix_threshold
+                new_len = pix_threshold
+
+            dim = (new_wid, new_len)
+            self.hsv_img = cv2.resize(self.hsv_img, dim, interpolation=cv2.INTER_AREA)
+
+        print("preprocessing complete")
+        return
+
+    def calculateProperties(self):
+        # call all feature algorithms and return a list of feature scores normalized from 0 to 1
+
+        # TODO: remove once a feature is complete and integrated into driver
+        # self.properties_list = [0.5]*6
+
+        # call balance
+        # self.properties_list[0] = calcBalance(self.hsv_img)
+
+        # call emphasis
+        # self.properties_list[1] = calcEmphasis(self.img)
+
+        # call harmony
+        self.properties_list[2] = calcHarmony(self.hsv_img)
+
+        # call variety
+        self.properties_list[3] = calcVariety(self.hsv_img)
+
+        # call gradation
+        # self.properties_list[4] = calcGradation(self.hsv_img)
+
+        # call movement
+        # self.properties_list[5] = calcMovement(self.hsv_img)
+        print("properties calculations complete")
+        return
+
+    def getImage(self):
+        return self.img
+
+    def getHSVImage(self):
+        return self.hsv_img
+
+    def getPropertiesList(self):
+        return self.properties_list
 
 
 def genNeighborhoodHistogram(neighborhoodMatrix, setting='hue'):
